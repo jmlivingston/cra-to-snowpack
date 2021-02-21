8e04e881e01d33998269483589aef1ceca4309c8
jmlivingston <jmlivingston@users.noreply.github.com>
2021-02-21 02:57:44 +0000

    deploy: e3f3d4495a525609de3e553c147149ed2d56fff6

--- scripts/snowpack/snowpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  exclude: ['**/node_modules/**/*', '**/scripts/**/*.mjs'],
-  plugins: ['@snowpack/plugin-react-refresh'],
-  routes: [{ match: 'routes', src: '.*', public: '/', dest: 'public/index.html' }],
-}
-
-console.log(process.env)

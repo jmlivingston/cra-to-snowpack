0c9b0f160ba9756efb4de2c96988625f95e31e14
jmlivingston <jmlivingston@users.noreply.github.com>
2021-02-21 18:29:28 +0000

    deploy: 39546ac887a0673a3ffb801124d57cae15835b63

--- react-snowpack/dist/App.js
+++ react-snowpack/dist/App.js
@@ -1,4 +1,4 @@
-import React from "../_snowpack/pkg/react.js";
+import React from "../../_snowpack/pkg/react.js";
 import logo from "./logo.svg.proxy.js";
 import "./App.css.proxy.js";
 function App() {
